02c82ddc2ac34ae315eeed471d99d455aac442ca
Kamel R <kamel.r@wayne.edu>
2024-05-23 18:31:46 -0400

    added the online student class

--- OnlineStudent.cpp
+++ OnlineStudent.cpp
@@ -1,1 +1,21 @@
 #include "OnlineStudent.h"
+
+void OnlineStudent::setLocation(string loc)
+{
+	location = loc;
+}
+
+string OnlineStudent::getLocation()
+{
+	return location;
+}
+
+OnlineStudent::OnlineStudent():Student()
+{
+	setLocation("Not Set yet!");
+}
+
+OnlineStudent::OnlineStudent(string m, string n, string i, string loc):Student(m,n,i)
+{
+	setLocation(loc);
+}
